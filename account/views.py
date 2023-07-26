fe63c86bc109241be484b0e89cb3dd40a2c7dca4
Hare1901 <hare1901@yandex.ru>
2023-07-26 10:57:10 +0300

    Feat: Представление входа и выхода.

--- account/views.py
+++ account/views.py
@@ -1,9 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+
 from .forms import LoginForm
 
 
+@login_required
+def dashboard(request):
+
+    return render(
+        request,
+        'account/dashboard.html',
+        {'sections': "dashboard"}
+    )
 def user_login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
