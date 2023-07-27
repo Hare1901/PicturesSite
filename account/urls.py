880c1ceb99ab081cc490ad92365a0112e26007be
Hare1901 <hare1901@yandex.ru>
2023-07-27 18:59:59 +0300

    Feat: Представления смены пароля

--- account/urls.py
+++ account/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     #path('login/', views.user_login, name='login'),
+    # путь для логина- разлогинивания
     path(
         "login/",
         auth_views.LogoutView.as_view(),
@@ -14,6 +15,18 @@ urlpatterns = [
        "logout/",
        auth_views.LogoutView.as_view(),
        name='logout'
+    ),
+    # путь для смены пароля
+    path(
+        "password-change/",
+        auth_views.PasswordResetView.as_view(),
+        name='password_change'
+    ),
+    #PasswordResetDoneView - отображает сообщение о удачной смене пароля
+    path(
+        "password-change/done/",
+        auth_views.PasswordResetDoneView.as_view(),
+        name="password_change_done"
     ),
 
     path("", views.dashboard, name='dashboard')
