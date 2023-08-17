ab76c72df33f8b6d6980be69b52b4fa9a9f6d56c
Hare1901 <hare1901@yandex.ru>
2023-08-17 19:48:26 +0300

    Feat: Настройка социально аутентификации. Установка и настройка Python Social Auth, настройка хостов,  RunServerPlus для настройки https, а так же необходимых библиотек.

--- PicturesSite/settings.py
+++ PicturesSite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7-%gyr2ii%_qldhjiyw#q+yxq3t_!t3_07gr9xy!w6g%czmw0c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_django',
+    'django_extensions',
 
 ]
 
