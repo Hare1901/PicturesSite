b4fa66a0db0280b234405b382e2bed28c1291351
Hare1901 <hare1901@yandex.ru>
2023-08-28 06:12:16 +0300

    Feat: разработка формы регистрации изображения с проверкой валидности url

---
+++ images/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+
+from models import Image
+
+
+class ImageCreateForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields = ['title', 'url', 'description']
+        widgets = {
+            'url': forms.HiddenInput,
+        }
+
+    # Метод проверяет валиден ли переданнный в форму url адрес изображения
+    def clean_url(self):
+        url = self.cleaned_data['url']
+        valid_extensions = ['jpg', 'jpeg', 'png']
+        extension = url.rsplit('.', 1)[1].lower()
+        if extension not in valid_extensions:
+            raise forms.ValidationError("The given URL does not valid")
+
+        return url
