5b4bc04f5ec55778f700a9570c5b642dca75eb5b
Hare1901 <hare1901@yandex.ru>
2023-08-29 06:38:08 +0300

    Feat: Переопределение метода save() класса ModelForm, и создание представления формы create.html

--- images/forms.py
+++ images/forms.py
@@ -1,6 +1,11 @@
 from django import forms
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
+
+import requests
 
 from models import Image
+
 
 
 class ImageCreateForm(forms.ModelForm):
@@ -20,3 +25,20 @@ class ImageCreateForm(forms.ModelForm):
             raise forms.ValidationError("The given URL does not valid")
 
         return url
+
+    def save(self, force_insert=False,
+             force_update=False,
+             commit=True):
+        image = super().save(commit=False)
+        image_url = self.cleaned_data['url']
+        name = slugify(image.title)
+        extension = image_url.rsplit('.', 1)[1].lower()
+        image_name = f'{name}.{extension}'
+        # скачать изображение с данного URL-адреса
+        response = requests.get(image_url)
+        image.image.save(image_name,
+                         ContentFile(response.content),
+                         save=False)
+        if commit:
+            image.save()
+        return image
